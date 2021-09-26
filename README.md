3d39c2c2fd7bf4c5168e273a573adb86ad54d11b
Kholifatul Ardliyan <k.ardliyan@gmail.com>
2021-09-26 08:16:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 7 Days Flutter
+
+Repository ini merupakan tantangan slicing desain dari figma ke flutter selama tujuh hari.
---
+++ android/app/src/main/kotlin/com/example/seven_days_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.seven_days_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
